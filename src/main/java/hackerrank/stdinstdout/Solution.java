c449b7fc3d51d5cdbb6ce444fcd23f76d893a8ec
SandraRusu <sandra.rusu.ext@extendaretail.com>
2023-03-01 20:25:45 +0200

    Java Stdin and Stdout I

---
+++ src/main/java/hackerrank/stdinstdout/Solution.java
@@ -0,0 +1,18 @@
+package hackerrank.stdinstdout;
+
+import java.util.Scanner;
+
+public class Solution {
+
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            int c = scan.nextInt();
+
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
+        }
+
+}
