46e80c145ee79e542f27054e244c622d05242700
SandraRusu <sandra.rusu.ext@extendaretail.com>
2023-03-01 21:24:38 +0200

    Java Loops II

---
+++ src/main/java/hackerrank/loops/Solution2.java
@@ -0,0 +1,26 @@
+package hackerrank.loops;
+
+import java.util.Scanner;
+
+class Solution2 {
+    public static void main(String[] argh) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for (int i = 0; i < t; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int sum = a;
+            for (int j = 0; j < n; j++) {
+                int nextElement = (int) (Math.pow(2, j) * b);
+                sum = sum + nextElement;
+                System.out.print(sum + " ");
+            }
+
+            if (i < t - 1) {
+                System.out.print("\n");
+            }
+        }
+        in.close();
+    }
+}
