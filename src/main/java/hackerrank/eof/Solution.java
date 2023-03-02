014fe6129788dd619385b2bdf2422084f4d3985c
SandraRusu <sandra.rusu.ext@extendaretail.com>
2023-03-02 18:59:09 +0200

    Java Static Initializer Block

--- src/main/java/hackerrank/eof/Solution.java
+++ src/main/java/hackerrank/eof/Solution.java
@@ -4,14 +4,32 @@ import java.util.Scanner;
 
 public class Solution {
 
-    public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT.*/
-        Scanner scan = new Scanner(System.in);
-        int i = 1;
-        while (scan.hasNext()){
-            String s = scan.nextLine();
-            System.out.println(i + " " + s);
-            i++;
+    static int B;
+    static int H;
+    static boolean flag;
+
+
+    static {
+        flag = true;
+        Scanner scanner = new Scanner(System.in);
+        B = scanner.nextInt();
+        H = scanner.nextInt();
+        try {
+            if (B <= 0 || H <= 0) {
+                throw new Exception();
+            }
+        } catch (Exception e) {
+            System.out.println(e + ":" + "Breadth and height must be positive");
+            flag = false;
         }
     }
-}
+
+    public static void main(String[] args) {
+        if (flag) {
+            int area = B * H;
+            System.out.print(area);
+        }
+
+    }//end of main
+
+}//end of class
