0bfe70440cd505d6ec1066f707ee8151d3a957a5
SandraRusu <sandra.rusu.ext@extendaretail.com>
2023-03-03 18:00:24 +0200

    Java String Reverse

---
+++ src/main/java/hackerrank/strings/Solution3.java
@@ -0,0 +1,15 @@
+package hackerrank.strings;
+
+import java.util.Scanner;
+
+public class Solution3 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+
+        System.out.println((A.equals(new StringBuilder(A).reverse().toString()) ? "Yes" : "No"));
+
+    }
+}
