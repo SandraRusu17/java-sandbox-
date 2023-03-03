f5afbaeebfc7b364c59ce45e3c4ccee9f7867d45
SandraRusu <sandra.rusu.ext@extendaretail.com>
2023-03-03 16:31:03 +0200

    Java Substring

---
+++ src/main/java/hackerrank/strings/Solution1.java
@@ -0,0 +1,15 @@
+package hackerrank.strings;
+
+import java.util.Scanner;
+
+public class Solution1 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+
+        System.out.println(S.substring(start, end));
+    }
+}
