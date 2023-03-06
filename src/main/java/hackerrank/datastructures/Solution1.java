73474d0dc84a7075927b84956749648ec4cb1a34
SandraRusu <sandra.rusu.ext@extendaretail.com>
2023-03-06 21:03:25 +0200

    Java 1D Array

---
+++ src/main/java/hackerrank/datastructures/Solution1.java
@@ -0,0 +1,21 @@
+package hackerrank.datastructures;
+
+import java.util.Scanner;
+
+public class Solution1 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = scan.nextInt();
+        }
+        scan.close();
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
