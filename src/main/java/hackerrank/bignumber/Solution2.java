03fddff1f8bb8ee24e7ec28cd4e8691ada679801
SandraRusu <sandra.rusu.ext@extendaretail.com>
2023-03-06 19:43:19 +0200

    Java BigInteger

---
+++ src/main/java/hackerrank/bignumber/Solution2.java
@@ -0,0 +1,15 @@
+package hackerrank.bignumber;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Solution2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        BigInteger a = scan.nextBigInteger();
+        BigInteger b = scan.nextBigInteger();
+
+        System.out.println(a.add(b));
+        System.out.println(a.multiply(b));
+    }
+}
